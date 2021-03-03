1133e2377035717221e30bb62bafb63cfebc2067
Drew <drewharper29@gmail.com>
2021-03-03 08:07:01 -0500

    added services for dynamodb and controller to check status

---
+++ advertApi/advertApi/Controllers/Advert.cs
@@ -0,0 +1,72 @@
+﻿using advertApi.Services;
+using AdvertApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace advertApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class Advert : ControllerBase
+    {
+        private readonly IAdvertStorageService _advertStorageService;
+
+        public Advert(IAdvertStorageService advertStorageService)
+        {
+            _advertStorageService = advertStorageService;
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(201, Type=typeof(CreateAdvertResponse))]
+        public async Task<IActionResult> Create(AdvertModel model)
+        {
+            string recordId;
+
+            try
+            {
+                recordId = await _advertStorageService.Add(model);
+
+            }
+            catch(KeyNotFoundException)
+            {
+                return new NotFoundResult();
+            }
+            catch (Exception exception)
+            {
+
+                return StatusCode(500, exception.Message);
+                throw;
+            }
+
+            return StatusCode(201, new CreateAdvertResponse { Id = recordId });
+        }
+
+        [HttpPut]
+        [Route("Confirm")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200)]
+        public async Task<IActionResult> Confirm(ConfirmAdvertModel model)
+        {
+            try
+            {
+                await _advertStorageService.Confirm(model);
+            }
+            catch (KeyNotFoundException)
+            {
+                return new NotFoundResult();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+                throw;
+            }
+            return new OkResult();
+        }
+    }
+}
---
+++ advertApi/advertApi/Services/IAdvertStorageService.cs
@@ -0,0 +1,11 @@
+﻿using AdvertApi.Models;
+using System.Threading.Tasks;
+
+namespace advertApi.Services
+{
+    public interface IAdvertStorageService
+    {
+        Task<string> Add(AdvertModel model);
+        Task Confirm(ConfirmAdvertModel model);
+    }
+}
